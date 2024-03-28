c122e37a5671eff2bbb83e606cb12770e360530c
fernandonandoz <nandonet1998@gmail.com>
2024-03-27 20:36:02 -0400

    Adicionar arquivos de projeto.

---
+++ DesafioPratico/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DesafioPratico;
+
+class Program()
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Ola seja bem vindo!!");
+        Console.WriteLine("Queremos te dar boas vindas!!!! Informe seu nome: \n");
+
+        Console.Write("Para isso, Informe seu nome por favor: ");
+
+        string nomeInformado = Console.ReadLine();
+
+        Console.WriteLine("\n...................... \n");
+
+        Console.WriteLine("Olá " + nomeInformado + "! Seja muito bem-vindo!");
+    }
+}
